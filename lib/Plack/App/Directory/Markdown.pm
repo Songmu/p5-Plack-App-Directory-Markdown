fb4dc4521244899cf60f83c74940d974d4100283
Masayuki Matsuki <y.songmu@gmail.com>
2012-08-17 15:09:06 +0900

    initial commit

---
+++ lib/Plack/App/Directory/Markdown.pm
@@ -0,0 +1,32 @@
+package Plack::App::Directory::Markdown;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Plack::App::Directory::Markdown -
+
+=head1 SYNOPSIS
+
+  use Plack::App::Directory::Markdown;
+
+=head1 DESCRIPTION
+
+Plack::App::Directory::Markdown is
+
+=head1 AUTHOR
+
+Masayuki Matsuki E<lt>y.songmu@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut

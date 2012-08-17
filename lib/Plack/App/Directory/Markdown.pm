f28e9f56927286646c3134c1bc0e7d6a6508b491
Masayuki Matsuki <y.songmu@gmail.com>
2012-08-17 20:08:12 +0900

    indexもxslateにした

--- lib/Plack/App/Directory/Markdown.pm
+++ lib/Plack/App/Directory/Markdown.pm
@@ -13,35 +13,6 @@ use Text::Markdown::Discount qw/markdown/;
 use HTTP::Date;
 use URI::Escape;
 
-# Stolen from rack/directory.rb
-my $dir_file = "<tr><td class='name'><a href='%s'>%s</a></td><td class='size'>%s</td><td class='type'>%s</td><td class='mtime'>%s</td></tr>";
-my $dir_page = <<PAGE;
-<html><head>
-  <title>%s</title>
-  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-  <style type='text/css'>
-table { width:100%; }
-.name { text-align:left; }
-.size, .mtime { text-align:right; }
-.type { width:11em; }
-.mtime { width:15em; }
-  </style>
-</head><body>
-<h1>%s</h1>
-<hr />
-<table>
-  <tr>
-    <th class='name'>Name</th>
-    <th class='size'>Size</th>
-    <th class='type'>Type</th>
-    <th class='mtime'>Last Modified</th>
-  </tr>
-%s
-</table>
-<hr />
-</body></html>
-PAGE
-
 my $tx = Text::Xslate->new(
     path => [
         Data::Section::Simple->new->get_data_section,
@@ -76,7 +47,7 @@ sub serve_path {
         return $self->return_dir_redirect($env);
     }
 
-    my @files = ([ "../", "Parent Directory", '', '', '' ]);
+    my @files = ({ link => "../", name => "Parent Directory" });
 
     my $dh = DirHandle->new($dir);
     my @children;
@@ -98,21 +69,16 @@ sub serve_path {
             $basename .= "/";
             $url      .= "/";
         }
-
-        my $mime_type = $is_dir ? 'directory' : ( Plack::MIME->mime_type($file) || 'text/plain' );
-        push @files, [ $url, $basename, $stat[7], $mime_type, HTTP::Date::time2str($stat[9]) ];
+        push @files, { link => $url, name => $basename, mtime => HTTP::Date::time2str($stat[9]) };
     }
 
     my $path  = Plack::Util::encode_html("Index of $env->{PATH_INFO}");
-    my $files = join "\n", map {
-        my $f = $_;
-        sprintf $dir_file, map Plack::Util::encode_html($_), @$f;
-    } @files;
-    my $page  = sprintf $dir_page, $path, $path, $files;
-
+    my $page  = $tx->render('index.tx', {files => \@files});
+    $page = encode_utf8($page);
     return [ 200, ['Content-Type' => 'text/html; charset=utf-8'], [ $page ] ];
 }
 
+1;
 
 __DATA__
 
@@ -127,13 +93,22 @@ __DATA__
 </body>
 </html>
 
+@@ index.tx
+: cascade base;
+: override body -> {
+<ul>
+:   for $files -> $file {
+<li><a href="<: $file.link :>"><: $file.name :></a> (<: $file.mtime :>)</li>
+:   }
+</ul>
+: } # endblock body
+
 @@ md.tx
 : cascade base;
 : override body -> {
 : $content | mark_raw
 : } # endblock body
 
-1;
 __END__
 
 =head1 NAME

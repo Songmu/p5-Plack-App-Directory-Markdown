1518c8239581a08feb403cf1582a707ecf154b23
Masayuki Matsuki <y.songmu@gmail.com>
2012-09-08 22:39:18 +0900

    refactor pad-markdown.pl

--- bin/pad-markdown.pl
+++ bin/pad-markdown.pl
@@ -2,9 +2,9 @@
 use strict;
 use warnings;
 use utf8;
-use Getopt::Long;
+use Getopt::Long qw/GetOptions :config auto_help pass_through/;
 use Pod::Usage;
-use Plack::Loader;
+use Plack::Runner;
 
 use Plack::App::Directory::Markdown;
 
@@ -14,42 +14,32 @@ Plack::App::Diectory::Markdown kick start script.
 
 =head1 SYNOPSIS
 
-    % pad.pl
+    % pad-markdown.pl
 
     Options:
-        port=i
-        host=s
         root=s
         encoding=s
         title=s
         tx_path=s
         markdown_class=s
         markdown_ext=s
+        ...and plackup options
 
 =cut
 
-GetOptions(
-    \my %options, qw/
-        help
-        port=i
-        host=s
-        root=s
-        encoding=s
-        title=s
-        tx_path=s
-        markdown_class=s
-        markdown_ext=s
-    /,
-) or pod2usage(2);
-pod2usage(1) if $options{help};
-
-my $port = delete $options{port} || 9119;
-my $host = delete $options{host} || '0.0.0.0';
+GetOptions(\my %options, qw/
+    root=s
+    encoding=s
+    title=s
+    tx_path=s
+    markdown_class=s
+    markdown_ext=s
+/) or pod2usage(2);
 
 my $app = Plack::App::Directory::Markdown->new(%options)->to_app;
 
-Plack::Loader->auto(
-    port => $port,
-    host => $host,
-)->run($app);
+push @ARGV, '--port=9119' unless grep {/^(?:--port|-p)\b/} @ARGV;
+my $runner = Plack::Runner->new;
+$runner->parse_options(@ARGV);
+$runner->run($app);
 

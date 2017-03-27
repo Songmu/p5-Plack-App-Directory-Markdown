5fd85d30de82b4276b6107cfd5d7e0e1f169f220
Songmu <y.songmu@gmail.com>
2017-03-27 22:23:58 +0900

    Checking in changes prior to tagging of version 0.10.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index e8a946e..2e0a9fa 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for Perl extension Plack::App::Directory::Markdown
    
     {{$NEXT}}
    
    +0.10 2017-03-27T13:23:12Z
    +        - update script files
    +        - add callback option (bayashi++)
    +
     0.09 2015-01-11T16:13:16Z
             - Make the displayed path clickable so it can be used as navigation (ranguard)

--- lib/Plack/App/Directory/Markdown.pm
+++ lib/Plack/App/Directory/Markdown.pm
@@ -2,7 +2,7 @@ package Plack::App::Directory::Markdown;
 use strict;
 use warnings;
 use utf8;
-our $VERSION = '0.09';
+our $VERSION = '0.10';
 
 use parent 'Plack::App::Directory';
 use Encode qw/encode_utf8/;
